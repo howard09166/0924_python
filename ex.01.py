cbaa508a8998cb963e93c669f3be293b1c3ea72b
howard <D1546572@o365.fcu.edu.tw>
2026-09-26 19:56:10 +0800

    123

--- ex.01.py
+++ ex.01.py
@@ -4,3 +4,8 @@ print(f"{t//3600}:{t%3600//60:02}:{t%60:02}")
 all = int(input('全班'))
 x = int(input('繳交'))
 print(f"{x/all*100:.1f}%",'未繳交=',all-x)
+
+fir = int(input('第一次段考成績'))
+sec = int(input('第二次段考成績'))
+thi = int(input('第三次段考成績'))
+print(f"平均成績: {(fir + sec + thi) / 3:.1f}")
